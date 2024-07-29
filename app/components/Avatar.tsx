72d92e4c697af6a31ecdb2916105c7f32b6fab42
Quann0703 <tranquan07112003@gmail.com>
2024-07-29 17:53:36 +0700

    setup and nav

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,15 @@
+"use client";
+import Image from "next/image";
+const Avatar = () => {
+  return (
+    <Image
+      src={"/images/placeholder.svg"}
+      alt="avatar"
+      className="rounded-full "
+      height="30"
+      width="30"
+    />
+  );
+};
+
+export default Avatar;
